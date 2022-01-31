d66a383a5d94195ffcd4df49de5ab476035741fb
Simon San <14062932+simonsan@users.noreply.github.com>
2022-01-31 08:46:09 +0100

    Add dev note for future bulk import

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -20,9 +20,17 @@ if DEBUG:
 elif not DEBUG:
     logging.basicConfig(level=logging.INFO)
 
+cache_file_count = 0
+overall_number_of_cache_files = len(CACHE_FILES)
+
 for cache_file in CACHE_FILES:
 
-    LOGGER.info(f"Processing cache file: {cache_file}. Please wait ...")
+    cache_file_count += 1
+
+    LOGGER.info(
+        f"Processing cache file ({cache_file_count}/"
+        f"{overall_number_of_cache_files}): {cache_file}. Please wait ..."
+    )
 
     # Import data
     try:
@@ -41,12 +49,24 @@ for cache_file in CACHE_FILES:
         logging.basicConfig(level=logging.INFO)
 
     # Parsing
+    # TODO: We need to initialize the data_processor outside
+    # of the loop to keep our data file open and only write
+    # once as soon as we have processed all cache files
+    # NOTE: This is only needed for bulk import of many cache
+    # files at once (e.g. in case of reimporting all older
+    # cache files) so not really needed for now, but nice to
+    # have anyway
     data_processor = DataProcessor.new_with_collected_data(cache_data)
 
     # Update our data file
+
     try:
         if os.path.exists(PARQUET_FILE):
             LOGGER.info("Updating parquet file ...")
+            # TODO: When there are more than one cache file
+            # keep the parquet file open until we imported
+            # the last dataset and then write to disk
+            # saves time
             data_processor.update_parquet_file()
         else:
             LOGGER.info("Creating new parquet file ...")
